ac30725fecad3a6fdd22c2fbf69f5a97242b9e5b
tsioryrovantsoa <tsioryrovantsoa@gmail.com>
2023-11-12 14:42:26 +0300

    feat: avatar

--- resources/views/admin/include/navbar.blade.php
+++ resources/views/admin/include/navbar.blade.php
@@ -13,7 +13,8 @@
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
                     aria-label="Open user menu">
-                    <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
+                    <div class="avatar bg-blue text-blue-fg" data-demo-color="">akv
+                    </div>
                     <div class="d-none d-xl-block ps-2">
                         <div>{{ auth()->user()->name }}</div>
                         <div class="mt-1 small text-muted">Admin</div>
